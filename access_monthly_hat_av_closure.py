212d3b64e726cea28422428d3c67a6e7dd1d02e8
Chris Bladwell <c.bladwell@unsw.edu.au>
2021-07-14 17:10:49 +1000

    analysis scripts

---
+++ access_monthly_hat_av_closure.py
@@ -0,0 +1,57 @@
+import xarray as xr
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import sys
+
+
+mom_dir = r'/scratch/e14/cb2411/access-om2/archive/1deg_jra55_iaf/'
+
+run = sys.argv[1]
+
+
+
+
+def check_cell_closure(oheat, ora, ocean, z,y,x):
+    rho0 = 1035 # kg/m^3
+    Cp = 3992.1032232964 # J kg-1 degC-1
+    # check that averaged ocean_heat tendency and ocean temp are equal
+    mnth_tend_c1 = (oheat.temp_tendency)[:,z,y,x]
+    ra_temp_c1 = (ora.temp_tendency)[:,z,y,x]
+    mnth_temp_c1 = ocean.temp_rhodzt[:,z,y,x]*Cp
+
+    print('  i  |      ris av      |    mnth av   |   err')
+    for t0 in range(11):
+    #     dt = oheat.average_DT.values[t0].astype('timedelta64[s]')/ np.timedelta64(1, 's') # decode_times=True
+        dt = oheat.average_DT[t0].values*24*60*60
+        mnthly_ra_temp_c1 = mnth_tend_c1.values[t0]*dt+ra_temp_c1.values[t0+1]-ra_temp_c1.values[t0]
+        mnthly_av_temp_c1 = mnth_temp_c1.values[t0+1]-mnth_temp_c1.values[t0]
+        print(' {0:2d}  | {1:16.6f} | {2:12.2f} | {3:12.8f}'.format(t0,mnthly_ra_temp_c1,
+            mnthly_av_temp_c1, (mnthly_ra_temp_c1 - mnthly_av_temp_c1)/mnthly_ra_temp_c1))
+
+def check_global_av_closure(oheat, ora, ocean):
+    rho0 = 1035 # kg/m^3
+    Cp = 3992.1032232964 # J kg-1 degC-1
+    # check that averaged ocean_heat tendency and ocean temp are equal
+    mnth_tend = (oheat.temp_tendency).mean(dim=["xt_ocean","yt_ocean","st_ocean"])
+    ra_temp = (ora.temp_tendency).mean(dim=["xt_ocean","yt_ocean","st_ocean"])
+    mnth_temp = ocean.temp_rhodzt.mean(dim=["xt_ocean","yt_ocean","st_ocean"])*Cp
+    print('  i  |     ris av       |   mnth av    |      err     | total error')
+    for t0 in range(11):
+        dt = oheat.average_DT[t0].values*24*60*60
+        mnthly_ra_temp = mnth_tend.values[t0]*dt+ra_temp.values[t0+1]-ra_temp.values[t0]
+        mnthly_av_temp = mnth_temp.values[t0+1]-mnth_temp.values[t0]
+        print(' {0:2d}  | {1:16.6f} | {2:12.2f} | {3:12.8f} | {4:12.8f}'.format(t0, mnthly_ra_temp, mnthly_av_temp,
+                                                                    (mnthly_ra_temp - mnthly_av_temp)/mnthly_ra_temp,
+                                                                    mnthly_ra_temp - mnthly_av_temp))
+
+
+if __name__ == '__main__':
+    ora = xr.open_dataset(os.path.join(access_dir, run, 'ocean/ocean_risavg.nc'), decode_times=False)
+    ogrid = xr.open_dataset(os.path.join(access_dir, run, 'ocean/ocean_grid.nc'), decode_times=False)
+    ocean = xr.open_dataset(os.path.join(access_dir, run, 'ocean/ocean.nc'), decode_times=False)
+    oheat = xr.open_dataset(os.path.join(access_dir, run, 'ocean/ocean_heat.nc'), decode_times=False)
+    osnap = xr.open_dataset(os.path.join(access_dir, run, 'ocean/ocean_snap.nc'), decode_times=False)
+
+
+    check_cell_closure(oheat, ora, ocean, 0,100,100)
---
+++ hat_average_figures.py
@@ -0,0 +1,51 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+def global_average(data_dir, nc_name, diag_name):
+    """plot the vertically integrated diag"""
+    outputs = list_outputs(data_dir)
+    ds = xr.open_dataset(os.path.join(data_dir, outputs[0], nc_name), decode_times=False, chunks={"st_ocean":5})
+    glb_mean = ds["diag_name"].sum(dim=["st_ocean", "xt_ocean", "yt_ocean"])
+    for _ in outputs[1:]:
+        ds = xr.open_dataset(os.path.join(data_dir, _, ds), decode_times=False, chunks={"st_ocean":5})
+        glb_mean = xarray.concat([v_int, ds["diag_name"].sum(dim=["st_ocean", "xt_ocean", "yt_ocean")], "time")
+    return glb_mean
+
+def list_outputs(data_dir):
+    return [_ for _ in os.listdir(data_dir) if _.startswith("output")]
+
+
+def concat_vert_int(data_dir, nc_name, diag_name):
+    outputs = list_outputs(data_dir)
+    ds = xr.open_dataset(os.path.join(data_dir, outputs[0], nc_name), decode_times=False, chunks={"st_ocean":5})
+    v_int = ds["diag_name"].sum(dim="st_ocean")
+    for _ in outputs[1:]:
+        ds = xr.open_dataset(os.path.join(data_dir, _, ds), decode_times=False, chunks={"st_ocean":5})
+        v_int = xarray.concat([v_int, ds["diag_name"].sum(dim="st_ocean")], "time")
+    return v_int
+
+
+def plot_global_av(diags):
+    for _ in diags:
+        _.plot()
+    plt.xlabal("time")
+
+# def combine_hat_av(nc_ra, nc_name, diag_name):
+
+
+def plot_epoch_diff_map(vert_int_diag):
+    X,Y = np.meshgrid(vert_int_diag.xt_ocean, vert_int_diag.yt_ocean)
+    plt.pcolormesh(X,Y, vert_int_diag)
+    plt.savefig("~/temp" + vert_int_diag.name + "map")
+
+
+
+# ./gadi_jupyter -q express -n 1 -P e14
+
+
+if __name__ == "__main__":
+    data_dir = r"/scratch/e14/cb2411/access-om2/archive/1deg_jra55_iaf"
